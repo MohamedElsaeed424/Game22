eaec2ce58fa7521d5a1b14d3164ab6990619516c
helguindy <elguindyhabiba7@gmail.com>
2023-05-20 14:11:50 +0300

    Lat

--- src/views/StartGame.java
+++ src/views/StartGame.java
@@ -1,15 +1,18 @@
 package views;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 //import javafx.scene.effect.DropShadow;
 //import javafx.scene.effect.Shadow;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 //import javafx.scene.text.FontPosture;
 //import javafx.scene.text.FontWeight;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -27,19 +30,25 @@ public class StartGame extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("First scene");
         Button button = new Button("Start Game");
-        button.setFont(new javafx.scene.text.Font(80));
+        button.setFont(new Font(80));
          button.setStyle("-fx-border-color:#000000;-fx-border-width:5px;");
-         button.setFont(Font.font("Tahoma", FontWeight.BOLD, 80));
-       // Font horrorFont = Font.loadFont(getClass().getResourceAsStream("/path/to/horror/font.ttf"), 50);
-        //button.setFont(horrorFont);
-       // WebView webView = new WebView();
-       // WebEngine webEngine = webView.getEngine();
-       // webEngine.load("https://media.giphy.com/media/GrJLbDwSeKL8A/giphy.gif");
+         button.setFont(Font.font("Tahoma", FontWeight.BOLD, 100));
+       //  button.setAlignment(Pos.BOTTOM_CENTER);
+      //  Text text = new Text("Welcome to The Last of Us!");
+        //text.setX(10);
+        //text.setY(10);
+      //  text.setFont(Font.font("Ariel", FontWeight.EXTRA_BOLD, 60));
+        BorderPane border = new BorderPane();
+       // text.setTextAlignment(Pos.TOP_RIGHT);
         StackPane stack = new StackPane();
-        //stack.getChildren().add(webView);
         stack.getChildren().add(button);
-        Scene scene = new Scene(stack,900,900);
+       // stack.getChildren().add(text);
+      // border.getChildren().add(text);
+       //border.getChildren().add(button);
+        Scene scene = new Scene(stack,1500,1000);
+        //Scene scene1 = new Scene(border,900,900);
         primaryStage.setScene(scene);
+       // primaryStage.setScene(scene1);
         primaryStage.show();
 
 

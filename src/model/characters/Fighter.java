78b098e75e6aba99c142f5ea24971f0349f2b064
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-05-25 09:53:42 +0300

    dsasd

--- src/model/characters/Fighter.java
+++ src/model/characters/Fighter.java
@@ -1,25 +1,9 @@
 package model.characters;
 
-import exceptions.InvalidTargetException;
-import exceptions.NoAvailableResourcesException;
-import exceptions.NotEnoughActionsException;
-
 public class Fighter extends Hero {
 
-    //--------------------------Constructor--------------------------------------------------
-    public Fighter(String name, int maxHp, int attackDmg, int maxActions) {
-        super(name, maxHp, attackDmg, maxActions);
-    }
-
-    public void attack() throws NotEnoughActionsException, InvalidTargetException {
-        super.attack();
-        if (this.isSpecialAction() == false) {
-            this.setActionsAvailable(this.getActionsAvailable() - 1);
-        }
-    }
-
-    public void useSpecial() throws NotEnoughActionsException, InvalidTargetException, NoAvailableResourcesException {
-        super.useSpecial();
+    public Fighter(String name, int maxHp, int attackDamage, int maxActions) {
+        super(name, maxHp, attackDamage, maxActions);
     }
 
 }
